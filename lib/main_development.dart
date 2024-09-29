4dcb479dea50822f77ba91da434c06ce432554a6
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-29 20:35:33 +0300

    Done set up Firebase Configuration✅

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,23 +2,11 @@ import 'package:azkary_app/Azkary_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
-        ),
-      ),
-      home: const HomePage(
-        title: 'Development',
-      ),
-    );
-  }
+  runApp(
+    const MyApp(
+      title: 'Azkary Development',
+      color: Colors.amber,
+      isdebug: true,
+    ),
+  );
 }
