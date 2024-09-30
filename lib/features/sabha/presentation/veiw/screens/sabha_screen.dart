e850307b493a9eb12fbd40bdf7d046f888b29f1d
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-30 18:05:03 +0300

    create system design by use clean architecture

---
+++ lib/features/sabha/presentation/veiw/screens/sabha_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SabhaScreen extends StatelessWidget {
+  const SabhaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Sabha'),
+      ),
+    );
+  }
+}
