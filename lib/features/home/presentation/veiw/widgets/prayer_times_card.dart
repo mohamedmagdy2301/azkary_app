470fef57c5781ca6f00477633f8c4f1cc03cb7f9
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-06 02:17:46 +0300

    Added prayer times card

---
+++ lib/features/home/presentation/veiw/widgets/prayer_times_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrayerTimesCard extends StatelessWidget {
+  const PrayerTimesCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
