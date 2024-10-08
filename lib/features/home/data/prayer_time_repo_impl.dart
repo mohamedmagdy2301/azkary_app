fbbc218fa0b64b5c9f03b5d085ff8e218471c82c
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-08 03:29:49 +0300

    added timings for request api

--- lib/features/home/data/prayer_time_repo_impl.dart
+++ lib/features/home/data/prayer_time_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:azkary_app/features/home/data/prayer_times_model/prayer_times_model.dart';
 import 'package:azkary_app/features/home/domain/prayer_times_entity.dart';
 import 'package:dio/dio.dart';
@@ -6,13 +8,15 @@ class PrayerTimesRepository {
   Dio dio = Dio();
   static Map<String, String> timings = {};
   Future<List<PrayerTimesEntity>> fetchPrayerTimes() async {
-    const String apiUrl = 'https://api.aladhan.com/v1/timings';
+    const String apiUrl = 'https://api.aladhan.com/v1/timings/';
     const String latitude = '30.5632921';
     const String longitude = '30.9970544';
     const String method = '5';
     List<PrayerTimesEntity> dataListInfo = [];
+    DateTime now = DateTime.now();
+    String formattedDate = "${now.day}-${now.month}-${now.year}";
     Response response = await dio.get(
-      '$apiUrl?latitude=$latitude&longitude=$longitude&method=$method',
+      '$apiUrl$formattedDate?latitude=$latitude&longitude=$longitude&method=$method',
     );
     Map<String, dynamic> data = response.data;
     timings = Map<String, String>.from(data['data']['timings']);
