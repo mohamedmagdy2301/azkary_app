7349a107927b1b3e17818f83adc9b7256cc73907
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-08 04:24:14 +0300

    fix bugs when next time is fagr

--- lib/core/functions/get_status_prayer_time.dart
+++ lib/core/functions/get_status_prayer_time.dart
@@ -2,18 +2,18 @@ import 'dart:developer';
 
 import 'package:azkary_app/features/home/data/prayer_time_repo_impl.dart';
 
-findPrayerTimes() {
+String findPrayerTimes() {
   DateTime now = DateTime.now();
   List<DateTime> nextPrayerTime = [];
-  List<DateTime> pastPrayerTime = [];
+
+  // Loop through the prayer times and classify them as past or next
   PrayerTimesRepository.timings.forEach(
     (prayerName, prayerTimeString) {
-      if (prayerName == "Sunset" ||
-          prayerName == "Imsak" ||
-          prayerName == "Firstthird" ||
-          prayerName == "Lastthird" ||
-          prayerName == "Midnight") {
-      } else {
+      if (prayerName != "Sunset" &&
+          prayerName != "Imsak" &&
+          prayerName != "Firstthird" &&
+          prayerName != "Lastthird" &&
+          prayerName != "Midnight") {
         DateTime prayerTime = DateTime(
           now.year,
           now.month,
@@ -22,25 +22,46 @@ findPrayerTimes() {
           int.parse(prayerTimeString.split(":")[1]),
         );
 
-        if (now.isAfter(prayerTime)) {
-          pastPrayerTime.add(prayerTime);
-        } else {
+        // Classify the prayer times
+        if (now.isBefore(prayerTime)) {
           nextPrayerTime.add(prayerTime);
         }
       }
     },
   );
+
   if (nextPrayerTime.isEmpty) {
-    nextPrayerTime = pastPrayerTime;
-    pastPrayerTime = [];
+    PrayerTimesRepository.timings.forEach(
+      (prayerName, prayerTimeString) {
+        if (prayerName != "Sunset" &&
+            prayerName != "Imsak" &&
+            prayerName != "Firstthird" &&
+            prayerName != "Lastthird" &&
+            prayerName != "Midnight") {
+          DateTime prayerTime = DateTime(
+            now.year,
+            now.month,
+            now.day + 1, // Move to the next day
+            int.parse(prayerTimeString.split(":")[0]),
+            int.parse(prayerTimeString.split(":")[1]),
+          );
+          nextPrayerTime.add(prayerTime);
+        }
+      },
+    );
   }
-  // log("nextPrayer: $nextPrayerTime");
-  // log("pastPrayer: $pastPrayerTime");
+
+  // Calculate time difference
   Duration timeDifference = nextPrayerTime[0].difference(now);
   int hours = timeDifference.inHours;
   int minutes = timeDifference.inMinutes % 60;
   int seconds = timeDifference.inSeconds % 60;
-  log("currentPrayer: '$hours:$minutes:$seconds'");
 
-  return '$hours:$minutes:$seconds';
+  // Format the time difference with leading zeros if necessary
+  String formattedTime = "${hours.abs().toString().padLeft(2, '0')}:"
+      "${minutes.abs().toString().padLeft(2, '0')}:"
+      "${seconds.abs().toString().padLeft(2, '0')}";
+
+  log("Next prayer in: $formattedTime");
+  return formattedTime;
 }
